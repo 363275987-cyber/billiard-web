2ec41bd87c8aef036a876a42ef01775ccdd8586a
王孟南 <wangmengnan@wangmengnandeiMac.local>
2026-03-19 05:16:10 +0800

    fix(security): supabase key 改为环境变量 + 添加 eslint/prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+{
+  "root": true,
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "vue/multi-word-component-names": "off",
+    "vue/no-v-html": "off"
+  }
+}
